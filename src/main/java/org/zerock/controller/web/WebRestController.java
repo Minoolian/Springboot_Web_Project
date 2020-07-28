d0c876c7eb35bad3f9d326b019aa7c5a77031cf1
이민우 <mw.lee@kakao.com>
2020-07-28 16:10:43 +0900

    Nginx를 이용한 무중단 배포
    - Profile을 확인하는 API 작성: WebRestController
    - 사용자가 지정한 profile을 사용할 수 있도록 설정: .yml + Application.java

--- src/main/java/org/zerock/controller/web/WebRestController.java
+++ src/main/java/org/zerock/controller/web/WebRestController.java
@@ -1,6 +1,7 @@
 package org.zerock.controller.web;
 
 import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,11 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 import org.zerock.controller.dto.posts.PostsSaveRequestDto;
 import org.zerock.controller.service.PostsService;
 
+import java.util.Arrays;
+
 @RestController
 @AllArgsConstructor
 public class WebRestController {
 
     private PostsService postsService;
+    private Environment env;
 
     @GetMapping("/hello")
     public String hello(){
@@ -22,5 +26,12 @@ public class WebRestController {
     @PostMapping("/posts")
     public Long savePosts(@RequestBody PostsSaveRequestDto dto){
         return postsService.save(dto);
+    }
+
+    @GetMapping("/profile")
+    public String getProfile(){
+        return Arrays.stream(env.getActiveProfiles())
+                .findFirst()
+                .orElse("");
     }
 }

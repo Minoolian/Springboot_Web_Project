3529760fef7c0111770f51ef11823df9e71359fe
Minoolian <mw.lee@kakao.com>
2020-12-23 17:30:57 +0900

    Controller 작성 및 Test
    - Controller CRUD 작성
    - Test 작성 (미완)

--- src/main/java/org/zerock/controller/web/BoardController.java
+++ src/main/java/org/zerock/controller/web/BoardController.java
@@ -4,7 +4,10 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.zerock.controller.domain.board.Board;
 import org.zerock.controller.service.BoardService;
 
 @Controller
@@ -15,9 +18,31 @@ public class BoardController {
     private BoardService boardService;
 
     @GetMapping("/list")
-    public String list(Model model){
-
+    public String list(Model model) {
         model.addAttribute("list", boardService.findAll());
         return "board";
+    }
+
+    @PostMapping("/register")
+    public String register(Board board) {
+        boardService.saveBoard(board);
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("/get")
+    public void get(@RequestParam("bno") Long bno, Model model) {
+        model.addAttribute("board", boardService.findBoard(bno));
+    }
+
+    /*@PostMapping("/modify")
+    public String modify(Board board) {
+        boardService.updateBoard(board);
+        return "redirect:/board/list";
+    }*/
+
+    @PostMapping("/remove")
+    public String remove(@RequestParam("bno") Long bno) {
+        boardService.deleteBoard(bno);
+        return "redirect:/board/list";
     }
 }
--- src/test/java/org/zerock/controller/web/BoardControllerTests.java
+++ src/test/java/org/zerock/controller/web/BoardControllerTests.java
@@ -11,19 +11,27 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.zerock.controller.service.BoardService;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+//@WebMvcTest(BoardController.class)
+////단위테스트용
 @Slf4j
+//@EnableJpaAuditing
+//단위테스트용
 public class BoardControllerTests {
 
     @Autowired
+//    @MockBean
+//    단위테스트용
     private BoardService boardService;
 
     @Autowired
@@ -35,20 +43,46 @@ public class BoardControllerTests {
     @Before
     public void setup(){
         this.mockMvc= MockMvcBuilders.webAppContextSetup(ctx)
+                .addFilters(new CharacterEncodingFilter("UTF-8", true))
+                .alwaysDo(print())
                 .build();
     }
 
     @Test
-    public void testList() throws Exception{
+    public void bootListTest() throws Exception{
 
         log.info("=============MVC TEST START=====================");
 
         mockMvc.perform(get("/board/list"))
+                .andExpect(status().isOk())
                 .andDo(print());
 
         log.info("=============MVC TEST END=====================");
 
-
     }
+
+//    단위테스트용
+//    단위테스트를 위해서 임시의 Service 클래스를 이용해 JPA를 사용하지 않는 방향...
+//    @Test
+//    public void unitListTest() throws Exception{
+//        Board board = Board.builder()
+//                .title("test")
+//                .writer("testwriter")
+//                .content("testcontent")
+//                .build();
+//
+//        boardService.saveBoard(board);
+//
+//        given(boardService.findBoard(1L).get())
+//        .willReturn(board);
+//
+//        final ResultActions actions = mockMvc.perform(get("/board"))
+//                .andDo(print());
+//
+//        actions
+//                .andExpect(status().isOk())
+//                .andDo(print());
+//
+//    }
 
 }

aeec2e8321c373c4d80c6faa62319d8c51b2dfd0
Minoolian <mw.lee@kakao.com>
2021-02-09 20:38:54 +0900

    검색 처리
    - Spring Data Jpa의 Specification을 이용하여 검색조건 조합준비.

--- src/main/java/org/zerock/controller/domain/board/BoardRepository.java
+++ src/main/java/org/zerock/controller/domain/board/BoardRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long>{
 // 메서드 명을 조합하여 쿼리를 처리할 수 있다.
@@ -11,5 +13,7 @@ public interface BoardRepository extends JpaRepository<Board, Long>{
     @Transactional
     int deleteByBno(Long bno);
     // custom delete에는 @Transactional 어노테이션 부여
+
+    List<Board> findByTitleContainingOrWriterContaining(String title, String content);
 
 }

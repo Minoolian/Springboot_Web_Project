2fdeaeafcc4bc51a680899693f95df17d5bdcb16
Minoolian <mw.lee@kakao.com>
2021-02-10 13:06:10 +0900

    검색 처리
    - JpaSpecification<T>를 Repository에서 상속.

--- src/main/java/org/zerock/controller/domain/board/BoardRepository.java
+++ src/main/java/org/zerock/controller/domain/board/BoardRepository.java
@@ -1,13 +1,14 @@
 package org.zerock.controller.domain.board;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
-public interface BoardRepository extends JpaRepository<Board, Long>{
+public interface BoardRepository extends JpaRepository<Board, Long>, JpaSpecificationExecutor<Board> {
 // 메서드 명을 조합하여 쿼리를 처리할 수 있다.
 
     @Transactional
